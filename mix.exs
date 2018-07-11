abe163a0b25d4746cd22d3f843bb8087bcccd537
Eric Oestrich <eric@oestrich.org>
2018-07-11 00:12:28 -0400

    Extra mix metadata, preparation for publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Gossip.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ],
+      description: description(),
+      package: package(),
+      homepage_url: "https://gossip.haus/",
+      source_url: "https://github.com/oestrich/gossip-elixir"
     ]
   end
 
@@ -23,6 +31,18 @@ defmodule Gossip.MixProject do
     [
       {:poison, "~> 3.1"},
       {:websockex, "~> 0.4.0"}
+    ]
+  end
+
+  def description() do
+    "Client for the Gossip MUD network"
+  end
+
+  def package() do
+    [
+      maintainers: ["Eric Oestrich"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/oestrich/gossip-elixir"}
     ]
   end
 end
