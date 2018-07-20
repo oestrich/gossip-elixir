c9f9e38636e9ae39e70a548a4c460e1d2acf8d00
Eric Oestrich <eric@oestrich.org>
2018-07-19 21:25:03 -0500

    Catch timeout for the call, we don't want this to crash higher up
    This is a known potential offline area, so treat it as such

--- lib/gossip.ex
+++ lib/gossip.ex
@@ -66,16 +66,22 @@ defmodule Gossip do
   Send a tell to a remote game and player
   """
   def send_tell(sending_player, game_name, player_name, message) do
-    response = GenServer.call(Tells, {:tell, sending_player, game_name, player_name, message})
-    case response do
-      {:error, :offline} ->
+    try do
+      response = GenServer.call(Tells, {:tell, sending_player, game_name, player_name, message})
+
+      case response do
+        {:error, :offline} ->
+          {:error, :offline}
+
+        %{"status" => "success"} ->
+          :ok
+
+        %{"status" => "failure", "error" => error} ->
+          {:error, error}
+      end
+    catch
+      :exit, _ ->
         {:error, :offline}
-
-      %{"status" => "success"} ->
-        :ok
-
-      %{"status" => "failure", "error" => error} ->
-        {:error, error}
     end
   end
 
