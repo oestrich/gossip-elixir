18d7b1655595cded4aeac56e2dee2bf17682c809
Eric Oestrich <eric@oestrich.org>
2018-10-26 21:36:11 -0400

    Bump the version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gossip.MixProject do
   def project do
     [
       app: :gossip,
-      version: "0.6.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
