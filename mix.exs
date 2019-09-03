ad2195968c19f905f713790250a0b462503fc4b4
Eric Oestrich <eric@oestrich.org>
2019-09-03 16:05:10 -0600

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gossip.MixProject do
   def project do
     [
       app: :gossip,
-      version: "1.3.0",
+      version: "1.4.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
